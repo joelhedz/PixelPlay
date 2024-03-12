37a5f255658d769801652c651f0991fccd43aaf6
jonyValladares <jonathandavid1997@outlook.com>
2024-03-11 23:56:20 -0600

    VIDEO LOGIN

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,15 @@ const auth = getAuth(appFirebase)
 
 const Home  = ({correoUsuario}) => {
   return (
+    
+    
     <div>
+      
         <h2 className='text-center'>Bienvenido usuario {correoUsuario} <button className='btn btn-primary' onClick={() =>signOut(auth)} >Logout</button>
         </h2>
 
     </div>
+    
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,76 +1,90 @@
 import React, { useState } from 'react'
-import Imagen from '../assets/celular.png'
 import ImagenProfile from '../assets/profile.png'
+import VideoFondo from '../assets/video.mp4'
 import './App.css'
 import appFirebase from '../credenciales'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
 const auth = getAuth(appFirebase)
 
-
-
-
 const Login = () => {
-
-
   const [registrando, setRegistrando] = useState(false)
-  
-  const funcAutetication = async(e) =>{
-    e.preventDefault();
-    const correo = e.target.email.value;
-    const contraseña =e.target.password.value;
-    
+
+  const funcAutetication = async e => {
+    e.preventDefault()
+    const correo = e.target.email.value
+    const contraseña = e.target.password.value
+
     if (registrando) {
       try {
-        await createUserWithEmailAndPassword(auth,correo,contraseña)
+        await createUserWithEmailAndPassword(auth, correo, contraseña)
       } catch (error) {
-        alert("Asegurese que la contraseña tenga mas de 8 caracteres")
+        alert('Asegurese que la contraseña tenga mas de 8 caracteres')
       }
-    }
-    else {
+    } else {
       try {
-        await signInWithEmailAndPassword(auth,correo,contraseña)
+        await signInWithEmailAndPassword(auth, correo, contraseña)
       } catch (error) {
-        alert("El correo o la contraseña son incorrectos")
+        alert('El correo o la contraseña son incorrectos')
       }
-      await signInWithEmailAndPassword(auth,correo,contraseña)
+      await signInWithEmailAndPassword(auth, correo, contraseña)
     }
-
   }
 
-
+  console.log(VideoFondo)
   return (
+    <div>
+      <div className="VIDEOFONDO">
+        <video autoPlay muted loop>
+          <source src={VideoFondo} type="video/mp4" />
+        </video>
+      </div>
 
-    <div class="diagonal-gradient">
-      <div className='container'>
+      <div
+        className="row vh-100 justify-content-center align-items-center"
+        style={{ zIndex: '100' }}
+      >
+        <div className=" col-s-4 col-md-5 col-lg-5 col-xl-6">
+          <div className="padre">
+            <img src={ImagenProfile} alt="" className="estilo-profile" />
+            <form onSubmit={funcAutetication}>
+              <div className="row align-items-center">
+                <div className="Entradas">
+                  <input
+                    type="email"
+                    placeholder="Ingresar Email"
+                    className="cajaTexto"
+                    id="email"
+                    required
+                  />
+                  <input
+                    type="password"
+                    placeholder="Ingresar contraseña"
+                    className="cajaTexto"
+                    id="password"
+                    required
+                  />
 
-        <div className='row'>
-
-          {/*columna mas pequeña formulario*/}
-
-          <div className='col-md-4'>
-
-            <div className="padre">
-              <div className='card card-body shadow'>
-                <img src={ImagenProfile} alt="" className='estilo-profile' />
-                <form onSubmit={funcAutetication} >
-                  <div className='row'>
-                    <input type="text" placeholder='Ingresar Email' className='cajaTexto' id='email' />
-                    <input type="password" placeholder='Ingresar contraseña' className='cajaTexto' id='password'/>
-                    <button className='btnform' >{registrando ? "Registrate" : "Inicia sesion"}</button>
-                  </div>
-
-                </form>
-
-                <h4 className='texto' >{registrando ? "si ya tiene cuenta" : "No tienes cuenta"}
-                  <button className='btnswitch' onClick={() => setRegistrando(!registrando)}>{registrando ? "Inicia sesion" : "registrate"}</button>
-                </h4>
-
+                  <button className="btnform">
+                    {registrando ? 'Registrate' : 'Inicia sesion'}
+                  </button>
+                </div>
               </div>
-            </div>
-          </div>
-          {/*COLUMNA  MAS GRANDE*/}
-          <div className='col-md-8'>
-            <img src={Imagen} alt="" className='tamaño-imagen' />
+            </form>
+
+            <h4 className="texto">
+              {registrando ? 'si ya tiene cuenta' : '¿No tienes cuenta?'}
+              &nbsp;&nbsp;&nbsp;
+              <button
+                className="btnswitch"
+                onClick={() => setRegistrando(!registrando)}
+              >
+                {registrando ? 'Inicia sesion' : 'registrate'}
+              </button>
+            </h4>
           </div>
         </div>
       </div>
